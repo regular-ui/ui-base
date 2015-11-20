5758308e14c4a1e8b2a3b82361d73dafa47daf05
Yusen <rainforest92@126.com>
2015-11-20 22:04:15 +0800

    [added] seperate base js files from regular-ui

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    _: require('./src/_.js'),
+    ajax: require('./src/ajax.js'),
+    Component: require('./src/component.js'),
+    SourceComponent: require('./src/sourceComponent.js'),
+    // directive: require('./src/directive.js'),
+    filter: require('./src/filter.js'),
+    compatibility: require('./src/compatibility.js')
+}
